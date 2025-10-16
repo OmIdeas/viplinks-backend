60e9c2f929199169fbeda8a14b52333059adb36d
OmIdeas <124409604+OmIdeas@users.noreply.github.com>
2025-10-16 11:40:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use('/api/dashboard', requireAuth, dashboardRouter); 
 
 
@@ -806,6 +807,7 @@ app.get('/__debug/rooms', (req, res) => {
   const sockets = Array.from(io.of('/').sockets.keys());
   res.json({ ok: true, rooms, sockets });
 });
+
 
 
 
